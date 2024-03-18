9773d4883600610dca42837de7b431a18588e746
anonymous <123231119+yahiaali97@users.noreply.github.com>
2024-03-18 04:13:17 +0200

    tdd

---
+++ src/test/java/tests/T01RegistrationTestWithJavaFaker.java
@@ -0,0 +1,39 @@
+package tests;
+
+import base.TestBase;
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+import pages.P01HomePage;
+import pages.P02RegistrationPage;
+
+import static org.testng.Assert.assertTrue;
+
+public class T01RegistrationTestWithJavaFaker extends TestBase {
+
+    P01HomePage homeObject;
+    P02RegistrationPage registerObject;
+    Faker fakeData = new Faker();
+    String firstname = fakeData.name().firstName();
+    String lastname = fakeData.name().firstName();
+    String email = fakeData.internet().emailAddress();
+    String password = fakeData.number().digits(8).toString();
+
+
+    @Test
+    public void userRegistration() {
+
+        homeObject = new P01HomePage(driver);
+        registerObject = new P02RegistrationPage(driver);
+
+        homeObject.openRegisterPage();
+        registerObject.userRegistration(firstname, lastname, email, password);
+
+        System.out.println(firstname);
+        System.out.println(lastname);
+        System.out.println(email);
+        System.out.println(password);
+
+        assertTrue(driver.findElement(registerObject.resultMsg).getText()
+                .contains("Your registration completed"));
+    }
+}
